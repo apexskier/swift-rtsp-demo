7b1126ccaf46f0469751f4c5238d2c62a095a7f1
Cameron Little <cameron@camlittle.com>
2025-06-10 09:32:52 -0600

    Initial Commit

---
+++ homecamera/homecameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  homecameraApp.swift
+//  homecamera
+//
+//  Created by Cameron Little on 2025-06-10.
+//
+
+import SwiftUI
+
+@main
+struct homecameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ homecameraTests/homecameraTests.swift
@@ -0,0 +1,16 @@
+//
+//  homecameraTests.swift
+//  homecameraTests
+//
+//  Created by Cameron Little on 2025-06-10.
+//
+
+import Testing
+
+struct homecameraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
