ca8ec51d47e40302e74df91eb392bc2eba292e3b
Cameron Little <cameron@camlittle.com>
2025-06-10 16:11:50 -0600

    remove buggy converted files

--- homecamera/H264-RTSP-Server-iOS/H264_RTSP_Server_iOS.h
+++ homecamera/H264-RTSP-Server-iOS/H264_RTSP_Server_iOS.h
@@ -7,12 +7,6 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for H264_RTSP_Server_iOS.
-FOUNDATION_EXPORT double H264_RTSP_Server_iOSVersionNumber;
-
-//! Project version string for H264_RTSP_Server_iOS.
-FOUNDATION_EXPORT const unsigned char H264_RTSP_Server_iOSVersionString[];
-
 // In this header, you should import all the public headers of your framework using statements like #import <H264_RTSP_Server_iOS/PublicHeader.h>
 
 #import "RTSP/RTSPServer.h"
